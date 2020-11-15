dc9e6e5dd51ffca7fbc568779da29710c520d689
Neil Chowdhary <ashuchow@gmail.com>
2020-11-15 22:07:48 +0530

    Small change

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { Button, Navbar } from 'react-bootstrap'
+import { ListGroup } from 'react-bootstrap'
 function Home() {
     return (
         <div>
-            <ul>
-                <li>
-                <Link to="/sf" className="nav-link">Book Flights</Link>
-                </li>
-                <li>
-                <Link to="/sh" className="nav-link">Book Hotels</Link>
-                </li>
-            </ul>
+            <ListGroup horizontal className = "ctr">
+                <ListGroup.Item className= "bg-dark"><Link to="/sf" >Book Flights</Link></ListGroup.Item>
+                <ListGroup.Item className= "bg-dark"><Link to="/sh" >Book Hotels</Link></ListGroup.Item>
+            </ListGroup>
         </div>
     )
 }
