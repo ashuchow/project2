5e527db3858e8011310db9d06d5e416b3d007ea3
Neil Chowdhary <ashuchow@gmail.com>
2020-11-17 00:43:23 +0530

    completed oauth for google, need to incorporate in frontend

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,32 @@
-const express = require('express');
-const authRoutes = require('./Routes/auth-routes');
-const passportSetup = require('./Config/passport-setup');
-const mongoose = require('mongoose');
+const express = require("express");
+const authRoutes = require("./Routes/auth-routes");
+const profileRoutes = require("./Routes/profile-routes");
+const passportSetup = require("./Config/passport-setup");
+const mongoose = require("mongoose");
 require("dotenv").config();
-//const keys = require('./config/keys');
+const keys = require("./Config/keys");
+const cookieSession = require("cookie-session");
+const passport = require('passport')
+
+
 
 const app = express();
 
 // set view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey],
+  })
+);
+
+//initialize passport
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 
 // connect to mongodb
 const uri = process.env.ATLAS_URI;
@@ -19,13 +37,15 @@ connection.once("open", () => {
 });
 
 // set up routes
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
+
+app.use("/profile", profileRoutes);
 
 // create home route
-app.get('/', (req, res) => {
-    res.render('home');
+app.get("/", (req, res) => {
+  res.render("home", {user: req.user});
 });
 
 app.listen(5000, () => {
-    console.log('app now listening for requests on port 5000');
+  console.log("app now listening for requests on port 5000");
 });
