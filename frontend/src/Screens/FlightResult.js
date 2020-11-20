f8c54ddf034e23a6c0c41f24bae4fb6c941886c1
Aditya Sarkar <adityavsarkar@gmail.com>
2020-11-21 04:00:44 +0530

    Flight search and result made roughly

--- frontend/src/Screens/FlightResult.js
+++ frontend/src/Screens/FlightResult.js
@@ -1,11 +1,49 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import {Table} from 'react-bootstrap';
+
 function resultFlight() {
     return (
+    <div>
+        <div>
+        <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Flight Name</th>
+      <th>Arrival City</th>
+      <th>Departure City</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td colSpan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</Table>
+        
+        </div>
         <div>
             <h1>Show Flight Search Results</h1>
             <Link to="/bf" className="nav-link">Flight XYZ</Link>
         </div>
+
+
+    </div>
     )
 }
 
--- frontend/src/Screens/FlightSearch.js
+++ frontend/src/Screens/FlightSearch.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {DropdownButton} from 'react-bootstrap';
-import Dropdown from 'react-bootstrap/Dropdown'
-
+import {Form} from 'react-bootstrap';
 
 
   
@@ -14,30 +12,29 @@ function searchFlight() {
             <h1>Search Flights</h1>
             
         </div>
-        <div className = "flex-container">
-            <div className = "flex-child arrival">
-            <DropdownButton id="dropdown-basic-button" title="From:">
-            <Dropdown.Item href="#/action-1">New Delhi</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Mumbai</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Kolkata</Dropdown.Item> 
-            <Dropdown.Item href="#/action-1">Bengaluru</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Ludhiana</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Chennai</Dropdown.Item> 
-            </DropdownButton>
-            
-            </div>
-            <div className = "flex-child destination">
-            <DropdownButton id="dropdown-basic-button" title="To:">
-            <Dropdown.Item href="#/action-1">New Delhi</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Mumbai</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Kolkata</Dropdown.Item> 
-            <Dropdown.Item href="#/action-1">Bengaluru</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Ludhiana</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Chennai</Dropdown.Item> 
-            </DropdownButton>
-            
-            </div>
-            
+        <div>
+    <Form>
+        <Form.Group controlId="exampleForm.ControlSelect1">
+        <Form.Label>Source City</Form.Label>
+        <Form.Control as="select">
+        <option>Kolkata</option>
+        <option>Delhi</option>
+        <option>Banglore</option>
+        <option>Mumbai</option>
+        <option>Chennai</option>
+        </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlSelect1">
+        <Form.Label>Destination City</Form.Label>
+        <Form.Control as="select">
+        <option>Kolkata</option>
+        <option>Delhi</option>
+        <option>Banglore</option>
+        <option>Mumbai</option>
+        <option>Chennai</option>
+        </Form.Control>
+        </Form.Group>
+    </Form>    
         </div>
         <div>
         <Link to="/rf" className="nav-link">Results</Link>
