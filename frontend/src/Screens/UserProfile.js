369084ea9fd17be1abb716947f81cce942b8e39e
Neil Chowdhary <ashuchow@gmail.com>
2020-11-25 22:49:09 +0530

    incorporated usersignup

--- frontend/src/Screens/UserProfile.js
+++ frontend/src/Screens/UserProfile.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Table } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import Userprofile from "../components/userprofile";
 
-function userProfile() {
+import FlightHistory from "../components/flighthistory";
+
+class UserProf extends Component {
+  render() {
     return (
-        <div>
-            <h1>User Profile Page</h1>
-        </div>
-    )
+ 
+
+      <div>
+        <Userprofile
+        name="Dev"
+        email="devghosh17@gmail.com"
+        phone="9831218674"
+        address="123 Road Road"
+        picture="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80"
+  
+        />
+        <h1>BOOKING HISTORY</h1>
+        <h1></h1>
+        <h1></h1>
+             <FlightHistory
+             flightname="Jet Airways 11"
+             acity="Kolkata"
+             dcity=" New Delhi"
+             atime="11:30"
+             dtime="3:30"
+             date="11.11.20"
+             price="Rs. 9000"
+            />
+
+      </div>
+    );
+  }
 }
 
-export default userProfile
+export default UserProf;
---
+++ frontend/src/components/userprofile.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Table } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
+class Userprofile extends Component {
+  render() {
+    return (   
+         <div className="col-md-4">
+      
+    <div className="d-flex flex-column align-items-center text-center">
+      <img className="float right" src={this.props.picture}alt="" className="img-fluid rounded shadow-sm"></img>
+     </div>
+  
+
+
+
+
+      <div className="card mb-4 align-items-right">
+        <div className="card-body">
+          <div className="row">
+            <div className="col-sm-4">
+              <h6 className="mb-0">Full Name</h6>
+            </div>
+            <div className="col-sm-10 text-secondary">
+              {this.props.name}
+            </div>
+          </div>
+          <hr/>
+          <div className="row">
+            <div className="col-sm-4">
+              <h6 className="mb-0">Email</h6>
+            </div>
+            <div className="col-sm-10 text-secondary">
+              {this.props.email}
+            </div>
+          </div>
+          <hr/>
+          <div className="row">
+            <div className="col-sm-4">
+              <h6 className="mb-0">Phone</h6>
+            </div>
+            <div className="col-sm-10 text-secondary">
+              {this.props.phone}
+            </div>
+          </div>
+          <hr/>
+          <div className="row">
+            <div className="col-sm-4">
+              <h6 className="mb-0">Address</h6>
+            </div>
+            <div className="col-sm-10 text-secondary">
+              {this.props.address}
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      
+      </div>
+      
+    );
+  }
+}
+
+export default Userprofile;
